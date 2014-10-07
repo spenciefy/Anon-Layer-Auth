8d9dbacb9b712699d5a110fcffbe1ad22fbb4f54
Spencer Yen <spencersyen@gmail.com>
2014-10-06 21:04:46 -0700

    osadf

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Requires
 var express = require('express');
 var app = express();
-var redis = require('redis');
-var db = redis.createClient();
+// var redis = require('redis');
+// var db = redis.createClient();
 var bodyParser = require('body-parser');
 var fs = require('fs');
 var r = require('jsrsasign');
@@ -16,33 +16,33 @@ var privateKey = fs.readFileSync('keys/layerkey.pem');
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-// app.post('/authenticate', function(req, res){
+app.post('/authenticate', function(req, res){
 
-//     var header =  JSON.stringify({
-//       typ: "JWS", // Expresses a MIMEType of application/JWS
-//       alg: "RS256", // Expresses the type of algorithm used to sign the token, must be RS256
-//       cty: "layer-eit;v=1", // Express a Content Type of application/layer-eit;v=1
-//       kid: layerKeyID,
-//     });
+    var header =  JSON.stringify({
+      typ: "JWS", // Expresses a MIMEType of application/JWS
+      alg: "RS256", // Expresses the type of algorithm used to sign the token, must be RS256
+      cty: "layer-eit;v=1", // Express a Content Type of application/layer-eit;v=1
+      kid: layerKeyID,
+    });
 
-//     var currentTimeInSeconds = Math.round(new Date() / 1000);
-//     var expirationTime = currentTimeInSeconds + 10000;
+    var currentTimeInSeconds = Math.round(new Date() / 1000);
+    var expirationTime = currentTimeInSeconds + 10000;
 
-//     var claim = JSON.stringify({
-//       iss: layerProviderID, // The Layer Provider ID
-//       prn: req.body.userID, // User Identifier
-//       iat: currentTimeInSeconds, // Integer Time of Token Issuance 
-//       exp: expirationTime, // Integer Arbitrary time of Token Expiration
-//       nce: req.body.nonce, //Nonce obtained from the Layer Client SDK
-//     });
+    var claim = JSON.stringify({
+      iss: layerProviderID, // The Layer Provider ID
+      prn: req.body.userID, // User Identifier
+      iat: currentTimeInSeconds, // Integer Time of Token Issuance 
+      exp: expirationTime, // Integer Arbitrary time of Token Expiration
+      nce: req.body.nonce, //Nonce obtained from the Layer Client SDK
+    });
 
-//     console.log(new r.Signature());
+    console.log(new r.Signature());
 
-//     var jws = r.jws.JWS.sign('RS256', header, claim, privateKey.toString());
+    var jws = r.jws.JWS.sign('RS256', header, claim, privateKey.toString());
 
-//      res.json({'identityToken': 'aaksldjfa3lkjfslkdjf'})
+     res.json({'identityToken': 'aaksldjfa3lkjfslkdjf'})
 
-//  });
+ });
 
 app.get('/', function(request, response) {
   response.send('aakash is a noob!!    and alex')
