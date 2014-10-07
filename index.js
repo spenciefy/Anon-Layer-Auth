073993109b6aee3c49d7f8c596ae880187478ff3
Spencer Yen <spencersyen@gmail.com>
2014-10-06 20:15:34 -0700

    testing

--- index.js
+++ index.js
@@ -16,29 +16,30 @@ app.use(bodyParser());
 
 app.post('/authenticate', function(req, res){
 
-    var header =  JSON.stringify({
-      typ: "JWS", // Expresses a MIMEType of application/JWS
-      alg: "RS256", // Expresses the type of algorithm used to sign the token, must be RS256
-      cty: "layer-eit;v=1", // Express a Content Type of application/layer-eit;v=1
-      kid: layerKeyID,
-    });
+    // var header =  JSON.stringify({
+    //   typ: "JWS", // Expresses a MIMEType of application/JWS
+    //   alg: "RS256", // Expresses the type of algorithm used to sign the token, must be RS256
+    //   cty: "layer-eit;v=1", // Express a Content Type of application/layer-eit;v=1
+    //   kid: layerKeyID,
+    // });
 
-    var currentTimeInSeconds = Math.round(new Date() / 1000);
-    var expirationTime = currentTimeInSeconds + 10000;
+    // var currentTimeInSeconds = Math.round(new Date() / 1000);
+    // var expirationTime = currentTimeInSeconds + 10000;
 
-    var claim = JSON.stringify({
-      iss: layerProviderID, // The Layer Provider ID
-      prn: req.body.userID, // User Identifier
-      iat: currentTimeInSeconds, // Integer Time of Token Issuance 
-      exp: expirationTime, // Integer Arbitrary time of Token Expiration
-      nce: req.body.nonce, //Nonce obtained from the Layer Client SDK
-    });
+    // var claim = JSON.stringify({
+    //   iss: layerProviderID, // The Layer Provider ID
+    //   prn: req.body.userID, // User Identifier
+    //   iat: currentTimeInSeconds, // Integer Time of Token Issuance 
+    //   exp: expirationTime, // Integer Arbitrary time of Token Expiration
+    //   nce: req.body.nonce, //Nonce obtained from the Layer Client SDK
+    // });
 
-    console.log(new r.Signature());
+    // console.log(new r.Signature());
 
-    var jws = r.jws.JWS.sign('RS256', header, claim, privateKey.toString());
+    // var jws = r.jws.JWS.sign('RS256', header, claim, privateKey.toString());
 
-    res.json({'identityToken': jws})
+    res.json({'identityToken': 'aaksldjfa3lkjfslkdjf'})
+
 });
 
 app.get('/', function (req, res) {
