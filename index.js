2691e6997f6045c7de233e68c3b8c3aefb5bebfd
Spencer Yen <spencersyen@gmail.com>
2014-10-06 20:45:33 -0700

    asdfk

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var r = require('jsrsasign');
 // Layer Vars
 var layerProviderID = 'ebe40df2-19c1-11e4-a04f-a19800003b1a';
 var layerKeyID = 'f5a06916-4d9d-11e4-bfbd-c770000045cb';
-var privateKey = fs.readFileSync('keys/layerkey.pem');
+//var privateKey = fs.readFileSync('keys/layerkey.pem');
 
 
  // app.post('/authenticate', function(req, res){
