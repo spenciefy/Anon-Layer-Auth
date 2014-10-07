19e8c83e69e5e66c65f0ae8c1aa94a350b8434d5
Spencer Yen <spencersyen@gmail.com>
2014-10-06 20:05:59 -0700

    commented out stuff

--- index.js
+++ index.js
@@ -12,34 +12,34 @@ var layerProviderID = 'ebe40df2-19c1-11e4-a04f-a19800003b1a';
 var layerKeyID = 'f5a06916-4d9d-11e4-bfbd-c770000045cb';
 var privateKey = fs.readFileSync('keys/layerkey.pem');
 
-// app.use(bodyParser());
+app.use(bodyParser());
 
-// app.post('/authenticate', function(req, res){
+app.post('/authenticate', function(req, res){
 
-//     var header =  JSON.stringify({
-//       typ: "JWS", // Expresses a MIMEType of application/JWS
-//       alg: "RS256", // Expresses the type of algorithm used to sign the token, must be RS256
-//       cty: "layer-eit;v=1", // Express a Content Type of application/layer-eit;v=1
-//       kid: layerKeyID,
-//     });
+    var header =  JSON.stringify({
+      typ: "JWS", // Expresses a MIMEType of application/JWS
+      alg: "RS256", // Expresses the type of algorithm used to sign the token, must be RS256
+      cty: "layer-eit;v=1", // Express a Content Type of application/layer-eit;v=1
+      kid: layerKeyID,
+    });
 
-//     var currentTimeInSeconds = Math.round(new Date() / 1000);
-//     var expirationTime = currentTimeInSeconds + 10000;
+    var currentTimeInSeconds = Math.round(new Date() / 1000);
+    var expirationTime = currentTimeInSeconds + 10000;
 
-//     var claim = JSON.stringify({
-//       iss: layerProviderID, // The Layer Provider ID
-//       prn: req.body.userID, // User Identifier
-//       iat: currentTimeInSeconds, // Integer Time of Token Issuance 
-//       exp: expirationTime, // Integer Arbitrary time of Token Expiration
-//       nce: req.body.nonce, //Nonce obtained from the Layer Client SDK
-//     });
+    var claim = JSON.stringify({
+      iss: layerProviderID, // The Layer Provider ID
+      prn: req.body.userID, // User Identifier
+      iat: currentTimeInSeconds, // Integer Time of Token Issuance 
+      exp: expirationTime, // Integer Arbitrary time of Token Expiration
+      nce: req.body.nonce, //Nonce obtained from the Layer Client SDK
+    });
 
-//     console.log(new r.Signature());
+    console.log(new r.Signature());
 
-//     var jws = r.jws.JWS.sign('RS256', header, claim, privateKey.toString());
+    var jws = r.jws.JWS.sign('RS256', header, claim, privateKey.toString());
 
-//     res.json({'identityToken': jws})
-// });
+    res.json({'identityToken': jws})
+});
 
 app.get('/', function (req, res) {
   res.send('Anon layer identity token generator (do i even know how to do this)');
